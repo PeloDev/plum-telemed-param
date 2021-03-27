3719f531303523db87847af82d98e062634a4159
Koketso <46706979+Teckoketso@users.noreply.github.com>
2021-03-27 06:45:03 +0200

    Update features.js

---
+++ src/features/features.js
@@ -0,0 +1,15 @@
+//import "./styles.css";
+import React from 'react';
+import { Image, Card, Container, Row, Col, Button } from "react-bootstrap";
+
+import Footer from '../footer/footer';
+
+  
+function Features() {
+  return (
+  
+  <Footer />
+  );
+}
+  
+export default Features;
