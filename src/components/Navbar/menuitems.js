59e28976271e63e448c31fc175fef6bb5184a5e6
“Boipelo <boipelo.zuma@webparam.org>
2021-03-28 15:30:38 +0200

    updated feature menu options

--- src/components/Navbar/menuitems.js
+++ src/components/Navbar/menuitems.js
@@ -13,7 +13,7 @@ const menuItems = [
     getMenuItem({key: 0,route: "about", label: "About", options: ["Who We Are", "Leadership \"Our Leaders\""]}),
     getMenuItem({key: 1,route: "services", label: "Our Services", options: ["Our Services"]}),
     getMenuItem({key: 2,route: "whyplum", label: "Why Plum Telemed", options: ["Who We Are"]}),
-    getMenuItem({key: 3,route: "features", label: "Features"}),
+    getMenuItem({key: 3,route: "features", label: "Features", options: ["Features Generic", "Communication Methods", "Remote Patient Monitoring"]}),
     getMenuItem({key: 4,route: "contactus", label: "Contact Us"}),
     getMenuItem({key: 5,route: "login", label: "Log In", isButton: true}),
     //getMenuItem({key: 6,route: "signup", label: "Sign Up", isButton: true}),
